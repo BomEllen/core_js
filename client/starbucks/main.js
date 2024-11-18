a7096de5c6a296cc5be84e5870f029a6ab266e30
kindtiger <seonbeom2@gmail.com>
2024-11-18 14:29:46 +0900

    learn. core javascript
    - 스타벅스 메뉴 (완성)

--- client/starbucks/main.js
+++ client/starbucks/main.js
@@ -11,43 +11,53 @@ const h = (t) => {
   t.style.height = 0
 }
 
-// header의 높이값을 가져와서 depth(3)의 top값으로 설정.
-// elem.offsetHeight
-// elem.style.top
+// depth top값 설정 코드 
 
-depthList.forEach(d=>d.style.top = header.offsetHeight + 'px')
-
+function vanilla(){
+  depthList.forEach(d=>d.style.top = header.offsetHeight + 'px')
 
 
+  aList.forEach( (a) => {
 
+    a.addEventListener('mouseenter',(e) => {
+      
+      const currentDepth = e.currentTarget.lastElementChild;
 
+      depthList.forEach(h)
 
+      currentDepth.style.height = '100px'
+      
+    })
 
-
-aList.forEach( (a) => {
-
-  // event binding => 'mouseenter' event => this 출력하기
-
-  // '현재 선택'된 a태그 안에 있는 depth
-  // currentTarget + lastElementChild
-
-  // currentDepth에게 높이 주기 (100px)
-  // node.style.height = ???
-
-  // 모든 depth 높이 제거하기 (0px)
-  // 모든 depth를 가져오기 => style 제거
-
-  a.addEventListener('mouseenter',(e) => {
-    
-    const currentDepth = e.currentTarget.lastElementChild;
-
-    depthList.forEach(h)
-
-    currentDepth.style.height = '100px'
-    
   })
 
+
+  header.addEventListener('mouseleave',()=>{
+    depthList.forEach(h)
+  })
+}
+
+
+
+
+/* global gsap */
+
+
+gsap.set(depthList,{top: header.offsetHeight})
+
+
+aList.forEach((a)=>{
+
+  const currentDepth = a.lastElementChild;
+
+  const tl = gsap.timeline({paused:true})
+              .to(currentDepth,{height:100,ease:'power2.inOut'});
+
+  a.addEventListener('mouseenter',()=> tl.play())
+  a.addEventListener('mouseleave',()=> tl.reverse())
+
 })
+
 
 
 // mouseleave => 원래대로
