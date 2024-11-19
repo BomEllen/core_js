04db23eaa52e8fa58ff0023ed263ebeef46aa8c4
kindtiger <seonbeom2@gmail.com>
2024-11-19 14:41:33 +0900

    learn. core javascript
    - calculator

--- client/main.js
+++ client/main.js
@@ -1,13 +1,62 @@
 
 
-const a = "hello";
+
+
+// 1. input 선택하기
+// 2. input 이벤트 바인딩
+// 3. input의 value 값 가져오기
+// 4. 숫자 더하기 
+// 5. result에 출력하기 
+
+
+/* global clearContents */
+
+const first = getNode('#firstNumber');
+const second = getNode('#secondNumber');
+const result = getNode('.result');
 
 
 
+function handleInput(){
+  const firstValue = Number(first.value);
+  const secondValue = +second.value;
+  const total = firstValue + secondValue 
 
-function sum() {
-
-  console.log(1 + 2);
-
-
+  
+  clearContents(result);
+  insertLast(result,total)
+  
 }
+
+first.addEventListener('input',handleInput)
+second.addEventListener('input',handleInput)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
