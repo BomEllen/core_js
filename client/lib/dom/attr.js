61abf2a8a7df393bed0edd912e807cee03f5db66
BomEllen <jjjj200100@gmail.com>
2024-11-20 08:45:43 +0900

    11/19일 학습 내용

--- client/lib/dom/attr.js
+++ client/lib/dom/attr.js
@@ -1,39 +1,40 @@
-function getAttr(node, prop) {
-  if (isString(node)) node = getNode(node);
+import { isString, isUndefined } from '../utils/type.js';
+import { getNode } from "./getNode.js";
+import { typeError } from "../error/typeError.js";
 
-  if (!isString(prop))
-    throw typeError(
-      'getAttr 함수에 전달된 두 번째 인수는 문자 타입 이어야 합니다.'
-    );
+function getAttr(node,prop){
 
-  return node.getAttribute(prop);
+  if(isString(node)) node = getNode(node);
+
+  if(!isString(prop)) throw typeError('getAttr 함수에 전달된 두 번째 인수는 문자 타입 이어야 합니다.')
+
+
+  return node.getAttribute(prop)
+
 }
 
 
-function setAttr(node, prop, value) {
-  if (isString(node)) node = getNode(node);
-  if (!isString(prop))
-    throw typeError('setAttr 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
-  if (isUndefined(value) || value === '') {
+function setAttr(node,prop,value){
+  
+  if(isString(node)) node = getNode(node);
+  if(!isString(prop)) throw typeError('setAttr 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
+  if(isUndefined(value) || value === ''){
+    
     node.removeAttribute(prop);
     return;
   }
 
-  if (prop.startsWith('data')) {
+  if( prop.startsWith('data') ) {
     prop = prop.slice(5);
     node.dataset[prop] = value;
     return;
   }
 
-  node.setAttribute(prop, value);
+  node.setAttribute(prop,value);
+
 }
 
 
-
-
-//attr('.about', 'id')  // getter
-// attr('.about', 'id', 'star')  // setter
-// 위 값이 나오도록 게터와 세터 합친 함수 만들기
 // function attr(node,prop,value){
   
 //   if(!value){
@@ -43,12 +44,21 @@ function setAttr(node, prop, value) {
 //   }
 // }
 
+export  const attr = (node,prop,value) => !value ? getAttr(node,prop) : setAttr(node,prop,value)
+
+// attr('.about','id') // getter
+// attr('.about','id','star') // setter
 
 
-// 위 함수를 에로우 펑션으로
 
-  const attr = (node, prop, value) =>
-  !value ? getAttr(node, prop) : setAttr(node, prop, value);
+
+
+
+
+
+
+
+
 
 
 
--- client/main.js
+++ client/main.js
@@ -1,5 +1,43 @@
-//alert('경고!!!!!!!!!');
 
-// confirm('정말 지워....?')
+// named export        => import { } from '..'
+// 얘도 알리하스로 이름 바꿀 수 있긴 함.
 
-// prompt('당신은 잘 생겼습니까.')
+// default export      => export default로 내보내서 중괄호 없이 받을 수 있다. import ... from '..'
+// 내보내는게 하나밖에 없을 때 내가 원하는 이름으로 받을 수 있다. 어차피 그 파일에서 내보내는건 하나니까.
+
+
+// import {getNode, getNodes} from './lib/dom/getNode.js';
+// import {insertLast} from './lib/dom/insert.js'
+// import clearContents from './lib/dom/clearContents.js';
+
+
+import { getNode, getNodes, typeError, insertLast, clearContents } from "./lib/index.js";
+
+
+
+
+
+
+// 1. input 선택하기
+// 2. input 이벤트 바인딩
+// 3. input의 value 값 가져오기
+// 4. 숫자 더하기
+// 5. result에 출력하기
+
+/* global clearContents */
+
+const first = getNode('#firstNumber');
+const second = getNode('#secondNumber');
+const result = getNode('.result');
+
+function handleInput() {
+  const firstValue = Number(first.value);
+  const secondValue = +second.value;
+  const total = firstValue + secondValue;
+
+  clearContents(result);
+  insertLast(result, total);
+}
+
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
