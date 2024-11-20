00759e979545a4486a95cf21cb9f88bd8e849214
BomEllen <jjjj200100@gmail.com>
2024-11-20 09:36:39 +0900

    11/20 계산기 마무리

--- client/main.js
+++ client/main.js
@@ -1,43 +1,83 @@
+// named export           =>  import { getNode as $, insertLast } from '..'
+// default export         =>  import ... from '..'
 
-// named export        => import { } from '..'
-// 얘도 알리하스로 이름 바꿀 수 있긴 함.
+// import { getNode as $, getNodes } from './lib/dom/getNode.js';
+// import { insertLast } from './lib/dom/insert.js';
 
-// default export      => export default로 내보내서 중괄호 없이 받을 수 있다. import ... from '..'
-// 내보내는게 하나밖에 없을 때 내가 원하는 이름으로 받을 수 있다. 어차피 그 파일에서 내보내는건 하나니까.
+// import clearContents from "./lib/dom/clearContents.js";
 
+import {
+  getNode as $,
+  getNodes,
+  typeError,
+  insertLast,
+  clearContents,
+} from './lib/index.js';
 
-// import {getNode, getNodes} from './lib/dom/getNode.js';
-// import {insertLast} from './lib/dom/insert.js'
-// import clearContents from './lib/dom/clearContents.js';
+function phase1() {
+  // 1. input 선택하기
+  // 2. input 이벤트 바인딩
+  // 3. input의 value 값 가져오기
+  // 4. 숫자 더하기
+  // 5. result에 출력하기
 
+  const first = $('#firstNumber');
+  const second = $('#secondNumber');
+  const result = $('.result');
+  const clear = $('#clear');
 
-import { getNode, getNodes, typeError, insertLast, clearContents } from "./lib/index.js";
+  function handleInput() {
+    const firstValue = Number(first.value);
+    const secondValue = +second.value;
+    const total = firstValue + secondValue;
 
+    clearContents(result);
+    insertLast(result, total);
+  }
 
+  function handleClear(e) {
+    e.preventDefault();
 
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
 
-
-
-// 1. input 선택하기
-// 2. input 이벤트 바인딩
-// 3. input의 value 값 가져오기
-// 4. 숫자 더하기
-// 5. result에 출력하기
-
-/* global clearContents */
-
-const first = getNode('#firstNumber');
-const second = getNode('#secondNumber');
-const result = getNode('.result');
-
-function handleInput() {
-  const firstValue = Number(first.value);
-  const secondValue = +second.value;
-  const total = firstValue + secondValue;
-
-  clearContents(result);
-  insertLast(result, total);
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
 }
 
-first.addEventListener('input', handleInput);
-second.addEventListener('input', handleInput);
+
+
+function phase2() {
+  const calculator = $('.calculator');
+  const result = $('.result');
+  const clear = $('#clear');
+  const numberInputs = [...document.querySelectorAll('input:not(#clear)')];
+
+
+
+  function handleInput() {
+    const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+
+
+  function handleClear(e) {
+    e.preventDefault();
+
+    numberInputs.forEach(clearContents);
+    result.textContent = '-';
+  }
+
+
+
+  calculator.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+phase2();
