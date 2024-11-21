010b457ae552288382d0f84d74f26a5ec3e8c1cb
kindtiger <seonbeom2@gmail.com>
2024-11-21 11:51:01 +0900

    learn. 비동기 통신
    - xhr 함수 분리

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -10,36 +10,72 @@ const END_POINT = 'https://jsonplaceholder.typicode.com/users';
 // 4 : complete  => 성공 | 실패
 
 
-const xhr = new XMLHttpRequest();
 
 
-xhr.open('GET',END_POINT);
 
-
-xhr.addEventListener('readystatechange',()=>{
-
-  if(xhr.readyState === 4){
-
-    if(xhr.status >= 200 && xhr.status < 400){
-
-      console.log(xhr.response);
-
-    }else{
-
-      console.error('실패ㅜㅜ');
-      
-    }
-
+function xhr({
+  method = 'GET',
+  url = '',
+  success = null,
+  fail = null,
+  body = null,
+  headers = {
+    'Content-Type':'application/json',
+    'Access-Control-Allow-Origin':'*'
   }
+} = {}){
+   
+  
+  const xhr = new XMLHttpRequest();
 
+  
+  xhr.open(method,url);
+
+  // 객체의 key, value를 분리 (Object.entries)
+  // 반복문 (forEach)
+
+  // xhr.setRequestHeader(key,value)
+
+  xhr.addEventListener('readystatechange',()=>{
+    const {status,response,readyState} = xhr;
+
+    if(readyState === 4){
+
+      if(status >= 200 && status < 400){
+
+        const data = JSON.parse(response);
+        success(data)
+        
+      }else{
+        fail({message:'알 수 없는 오류가 발생했습니다.'})
+      }
+    }
+  })
+  xhr.send(JSON.stringify(body));
+}
+
+
+
+const obj = {
+  name:'tiger',
+  age:38
+}
+
+
+xhr({
+
+  url: END_POINT,
+  success: (data)=>{
+    console.log( data );
+  },
+  fail: ()=>{},
+
+  
 })
 
 
 
 
-
-
-xhr.send();
 
 
 
