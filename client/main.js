d4b3686f0bf23e4753189ec4ebd9fb699712ee36
kindtiger <seonbeom2@gmail.com>
2024-11-25 11:50:39 +0900

    learn. POST 통신
    - 실습

--- client/main.js
+++ client/main.js
@@ -16,12 +16,7 @@ import {
 
 
 const END_POINT = 'http://localhost:3000/users'
-
-
 const userCardInner = getNode('.user-card-inner');
-
-
-
 
 
 
@@ -76,16 +71,7 @@ async function renderUserList(){
 }
 
 
-
 renderUserList()
-
-
-
-// 1. user 데이터 fetch 해주세요.
-
-
-// 2. fetch 데이터 유저의 이름만 콘솔에 출력 
-
 
 
 function handleDeleteCard(e){
@@ -111,14 +97,55 @@ function handleDeleteCard(e){
 
 }
 
+
+
 userCardInner.addEventListener('click',handleDeleteCard)
 
 
 
 
+const createButton = getNode('.create');
+const cancelButton = getNode('.cancel');
+const doneButton = getNode('.done');
+
+// create 버튼을 선택한다.
+// 클릭 이벤트를 바인딩한다.
+// create에 open 클래스를 추가한다.
+
+
+function handleCreate(){
+  
+  // this.classList.add('open');
+  gsap.to('.pop',{autoAlpha:1})
+  
+}
+
+function handleCancel(e){
+  e.stopPropagation();
+  // createButton.classList.remove('open');
+  gsap.to('.pop',{autoAlpha:0})
+}
+
+createButton.addEventListener('click',handleCreate)
+cancelButton.addEventListener('click',handleCancel)
+
+
+// cancel 버튼을 선택한다.
+// 클릭 이벤트를 바인딩한다.
+// create에 open 클래스를 제거한다.
 
 
 
+
+
+// POST 통신을 해주세요.
+
+// 1. input의 value를 가져온다.
+// 2. value를 모아서 객체를 생성
+// 3. 생성 버튼을 누르면 POST통신을 한다.
+// 4. body에 생성한 객체를 실어보낸다.
+// 5. 카드 컨텐츠 비우기
+// 6. 유저카드 리랜더링
 
 
 
