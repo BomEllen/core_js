522f087ccc7b42371036999837cd0ba984887e5e
BomEllen <jjjj200100@gmail.com>
2024-11-28 13:02:49 +0900

    11/28 일 todolist 학습

--- client/main.js
+++ client/main.js
@@ -1,13 +1,21 @@
 import { Button } from './components/Button/Button.js';
+import { Card } from './components/Card/Card.js';
+import { Counter } from './components/Counter/Counter.js';
+import { TodoItem } from './components/TodoItem/TodoItem.js';
+import { TodoList } from './components/TodoList/TodoList.js';
 
 const app = document.getElementById('app');
 
 function defineElements() {
   customElements.define('custom-button', Button);
+  customElements.define('custom-card', Card);
+  customElements.define('custom-counter', Counter);
+  customElements.define('custom-todo', TodoList);
+  customElements.define('custom-todo-item', TodoItem);
 }
 
 defineElements();
 
-const buttonElement = document.createElement('custom-button');
+const todoElement = document.createElement('custom-todo');
 
-app.append(buttonElement);
+app.append(todoElement);
